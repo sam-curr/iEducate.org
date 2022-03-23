dfdd8503d36ac7ebd1a2d330d4b22742f1e20c90
sam-curr <102237293+sam-curr@users.noreply.github.com>
2022-03-23 21:25:57 +0530

    Add files via upload

---
+++ JavaScript/resp.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightNav')
+
+
+
+burger.addEventListener('click', ()=>){
+    rightnav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('v-class-resp');
+}
